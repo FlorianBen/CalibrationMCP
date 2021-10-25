96fe01d5e44f6a22e23b036382a61e1be38fdbdd
Florian Benedetti <florian.benedetti@cea.fr>
2021-10-25 14:49:13 +0200

    Use argparser

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-import logging
 import condmcp.autocond as ac
 from multiprocessing import Pool
+import argparse
+from itertools import repeat
 
 
-def task(ind):
-    conditionner = ac.Conditionner('MPOD:', str(ind), 'seq.csv')
+def task(ind, file):
+    conditionner = ac.Conditionner('MPOD:', str(ind), file)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('channels', metavar='N', type=int, nargs='+',
+                        help='Channel use for the conditioning')
+    parser.add_argument("-s", "--seq", help="Sequence file", required=True)
+    args = parser.parse_args()
+
+    channels = args.channels
+    fileseq = args.seq
+
     with Pool(4) as p:
-        p.map(task, [0])
+        p.starmap(task, zip(channels, repeat(fileseq)))
     return 0
 
 
